b02f4a756817e48470ff55cfcea440f848f50a5b
mzalog <95581915+mzalog@users.noreply.github.com>
2024-06-18 08:37:01 +0200

    Adding files

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import device_info_plus
+import firebase_analytics
+import firebase_core
+import firebase_messaging
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+}
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Programy\\Flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Pojemnik danych\\Repositories\\Flutter\\smart_medicine_dispecer_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Programy\\Flutter"
+  "PROJECT_DIR=D:\\Pojemnik danych\\Repositories\\Flutter\\smart_medicine_dispecer_app"
+  "FLUTTER_ROOT=C:\\Programy\\Flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Pojemnik danych\\Repositories\\Flutter\\smart_medicine_dispecer_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Pojemnik danych\\Repositories\\Flutter\\smart_medicine_dispecer_app"
+  "FLUTTER_TARGET=D:\\Pojemnik danych\\Repositories\\Flutter\\smart_medicine_dispecer_app\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9lNzZjOTU2NDk4ODQxZTFhYjQ1ODU3N2QzODkyMDAzZTU1M2U0ZjNjLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Pojemnik danych\\Repositories\\Flutter\\smart_medicine_dispecer_app\\.dart_tool\\package_config.json"
+)
